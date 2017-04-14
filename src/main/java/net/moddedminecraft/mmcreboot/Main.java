146f03026dd7806fd3746d5857e9d5729a0e4936
leelawd <leelawd123@gmail.com>
2017-04-14 05:45:41 +0100

    v2.0.3

--- src/main/java/net/moddedminecraft/mmcreboot/Main.java
+++ src/main/java/net/moddedminecraft/mmcreboot/Main.java
@@ -45,7 +45,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@Plugin(id = "mmcreboot", name = "MMCReboot", version = "2.0.2", authors = {"Leelawd93"})
+@Plugin(id = "mmcreboot", name = "MMCReboot", version = "2.0.3", authors = {"Leelawd93"})
 public class Main {
 
     @Inject
